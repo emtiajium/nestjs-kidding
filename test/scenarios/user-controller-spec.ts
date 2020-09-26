65e4087d632b65ba728da323c62f0875d4aed896
Emtiaj Hasan <ehasan@testreach.com>
2020-09-26 16:34:59 +0600

    installed jest

---
+++ test/scenarios/user-controller-spec.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import UserModule from '@/modules/User';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+
+describe('UserController', () => {
+  let app: INestApplication;
+  beforeAll(async () => {
+    const PORT = 9877;
+    async function bootstrap(): Promise<void> {
+      app = await NestFactory.create(UserModule);
+      await app.listen(PORT);
+    }
+    await bootstrap();
+  });
+
+  describe('root', () => {
+    it('should return "Hello World!"', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect(response => {
+          expect(response.text).toBe('Hello World!');
+        });
+    });
+  });
+});
