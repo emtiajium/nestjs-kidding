92887f332d24847f5a04f64238b4f23577690b61
Emtiaj Hasan <ehasan@testreach.com>
2020-09-26 13:31:52 +0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,146 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    sourceType: 'module',
+    ecmaVersion: '2020',
+  },
+  plugins: ['@typescript-eslint', 'unicorn', 'prettier', 'node'],
+  extends: [
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:unicorn/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'error',
+    'import/no-relative-parent-imports': 'error',
+    'unicorn/filename-case': [
+      'error',
+      {
+        cases: {
+          pascalCase: true,
+          kebabCase: true,
+        },
+      },
+    ],
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/no-null': 'off',
+    'unicorn/no-reduce': 'off',
+    'class-methods-use-this': ['error', { exceptMethods: ['map'] }],
+    complexity: ['error', 5],
+    curly: 'error',
+    'no-console': 'error',
+    'no-floating-decimal': 'error',
+    'no-implicit-coercion': 'error',
+    'no-implied-eval': 'error',
+    'no-invalid-this': 'error',
+    'no-magic-numbers': [
+      'error',
+      {
+        ignore: [0, 1, -1],
+      },
+    ],
+    'no-throw-literal': 'error',
+    'no-unused-expressions': [
+      'error',
+      {
+        allowShortCircuit: true,
+        allowTernary: true,
+      },
+    ],
+    'prefer-named-capture-group': 'error',
+    'require-unicode-regexp': 'error',
+    'no-confusing-arrow': [
+      'error',
+      {
+        allowParens: true,
+      },
+    ],
+    'no-plusplus': [
+      'error',
+      {
+        allowForLoopAfterthoughts: true,
+      },
+    ],
+    'no-underscore-dangle': [
+      'error',
+      {
+        allow: [],
+        allowAfterThis: true,
+        allowAfterSuper: true,
+        allowAfterThisConstructor: true,
+        enforceInMethodNames: false,
+        allowFunctionParams: false,
+      },
+    ],
+    'no-duplicate-imports': 'error',
+    'node/no-process-env': 'error',
+    'node/callback-return': 'error',
+    'node/global-require': 'error',
+    'node/no-mixed-requires': 'error',
+    'node/no-new-require': 'error',
+    'node/no-sync': 'error',
+    'prefer-arrow-callback': [
+      'error',
+      {
+        allowNamedFunctions: false,
+        allowUnboundThis: true,
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ['test/**/*.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        'no-magic-numbers': 'off',
+        'node/no-process-env': 'off',
+      },
+    },
+    {
+      files: [
+        'src/**/*{Service,Handler,Adapter,Controller,Middleware,Factory,Provider,Dispatcher,Consumer,Manager,Builder}.ts',
+      ],
+      rules: {
+        'class-methods-use-this': 'off',
+      },
+    },
+    {
+      files: [
+        'src/commons/queue/ConsumerManager.ts',
+        'src/commons/queue/EventDispatcher.ts',
+        'src/commons/queue/QueuePoller.ts',
+        'src/commons/queue/InfrastructureHelper.ts',
+        'src/commons/test/consumers/ConsumerTestBuilder.ts',
+      ],
+      rules: {
+        'node/no-process-env': 'off',
+        'no-param-reassign': 'off',
+        'new-cap': 'off',
+        'node/callback-return': 'off',
+      },
+    },
+    {
+      files: ['scripts/**/*.ts'],
+      rules: {
+        'node/no-process-env': 'off',
+        'no-console': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+      },
+    },
+  ],
+};
