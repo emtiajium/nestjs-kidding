b84a6b0489d12b945996ac5758bbc72899029fd6
emtiajium <271emtiaj@gmail.com>
2020-10-11 13:32:39 +0600

    removed explicite validation

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,10 +1,8 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import * as uuid from 'uuid';
 import User from '@/data-transfer-object/UserDto';
-import { validate } from 'class-validator';
 import users from '@/mocks/users';
 import EmailService from '@/services/EmailService';
-import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export default class UserService {
@@ -15,20 +13,10 @@ export default class UserService {
   }
 
   async createUser(user: User): Promise<User> {
-    await this.validateCreateUser(user);
     await this.emailService.sendAccountOpeningEmail(user.username);
     return {
       ...user,
       id: uuid.v4(),
     };
-  }
-
-  async validateCreateUser(user: User): Promise<boolean> {
-    // very bad approach but ok for now
-    const errors = await validate(plainToClass(User, user));
-    if (errors.length > 0) {
-      throw new BadRequestException(errors);
-    }
-    return true;
   }
 }
