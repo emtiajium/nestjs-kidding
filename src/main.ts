c788c856fe321410ed98d5c81a00f775bd0163e9
emtiajium <271emtiaj@gmail.com>
2020-11-28 10:58:00 +0600

    type added for the app

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import AppModule from '@/AppModule';
+import { INestApplication } from '@nestjs/common';
 
 const PORT = 9876;
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app: INestApplication = await NestFactory.create(AppModule);
   await app.listen(PORT);
 }
 bootstrap();
