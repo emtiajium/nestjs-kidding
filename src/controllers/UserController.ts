3d52b33e00f25b5c763f14b0c51d51d4c26c2470
imran <imranur8@gmail.com>
2020-12-19 09:25:46 +0600

    - added PUT and delete

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, Put, Delete } from '@nestjs/common';
 import UserService from '@/services/UserService';
 import User from '@/data-transfer-object/UserDto';
 import Logged from '@/logger/logged';
@@ -7,7 +7,7 @@ import Logged from '@/logger/logged';
 export default class UserController {
   // no need to create instance with `new` [this.userService = new UserService();]
   // magic of @Injectable()
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Get()
   getUsers(): Promise<User[]> {
@@ -18,5 +18,17 @@ export default class UserController {
   @Logged()
   async createUser(@Body() user: User): Promise<User> {
     return this.userService.createUser(user);
+  }
+
+  @Put(':id')
+  @Logged()
+  async updateUser(@Param('id') id: string, @Body() user: User): Promise<User> {
+    return this.userService.updateUser(id, user);
+  }
+
+  @Delete(':id')
+  @Logged()
+  async deleteUser(@Param('id') id: string): Promise<Boolean> {
+    return this.userService.deleteUser(id);
   }
 }
--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import * as uuid from 'uuid';
 import User from '@/data-transfer-object/UserDto';
 import users from '@/mocks/users';
 import EmailService from '@/services/EmailService';
-
+let usersData = users;
 @Injectable()
 export default class UserService {
-  constructor(private readonly emailService: EmailService) {}
+  constructor(private readonly emailService: EmailService) { }
 
   async getUsers(): Promise<User[]> {
     return users;
@@ -20,5 +20,32 @@ export default class UserService {
     };
     users.push(newUser);
     return newUser;
+  }
+
+  async updateUser(id: string, user: User): Promise<User> {
+    const { username } = user;
+    const updatedUser = await usersData.find(obj => obj.id === id);
+    if (!updatedUser) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    // save data
+    usersData = usersData.map(obj => {
+      if (obj.id === id) {
+        obj = { ...obj, username }
+      }
+      return obj;
+    });
+    return user;
+  }
+
+  async deleteUser(id: string): Promise<Boolean> {
+    const deletingUser = await usersData.find(obj => obj.id === id);
+    if (!deletingUser) {
+      throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+    }
+    // delete data
+    usersData = usersData.filter(obj => obj.id !== id);
+    console.log(usersData);
+    return true;
   }
 }
