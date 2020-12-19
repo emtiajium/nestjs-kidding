256403ca273bb5264f0ebfa41dd5416a403d019a
emtiajium <271emtiaj@gmail.com>
2020-12-19 23:25:01 +0600

    fixed port if passed

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 
-const PORT = 0;
-async function bootstrap(module: any): Promise<INestApplication> {
+async function bootstrap(module: any, port = 0): Promise<INestApplication> {
   const app: INestApplication = await NestFactory.create(module);
   // to avoid putting @UsePipes(new ValidationPipe()) at every route
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(PORT);
+  await app.listen(port);
   const url = await app.getUrl();
   console.log(`URL: ${url.replace('[::1]', 'localhost')}`);
   return app;
